4f81e045c29c1499d82f6692f5d369cec63c8be8
bienvenueinde <76187368+bienvenueinde@users.noreply.github.com>
2023-03-17 20:26:49 +0100

    Add files via upload

---
+++ structs.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef double A;
+
+typedef struct {
+    A x;
+    A y;
+    A z;
+} Vector3;
+
+typedef struct {
+    Vector3 position;
+    A masse;
+    Vector3 vitesse_instantane;  
+} Poids;
+
+typedef struct {
+    A force_rappel;
+    A constante_rappel;
+    int longueur;
+} Ressorts;
+
+typedef struct {
+    A delta_t_;
+    Vector3 gravite;
+    int nmb_forces;
+    A *FE;
+} Env;
+
+Vector3 tableau(int x, int y, Ressorts R, Poids P) {
+   for(int i = 0; i < x; i++) {
+        for(int j = 0; j < y; j++) {
+            P.position.x = i * R.longueur;
+            P.position.y = j * R.longueur;
+            P.position.z = 0;
+        }
+    }
+    return P.position;
+}
+
+void main() {
+    /*int x = 5;
+    int y = 5;
+    Ressorts R;
+    R.longueur = 3;
+    Poids P;
+    Vector3 poids = tableau(x, y, R, P);*/
+}
