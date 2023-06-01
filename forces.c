05284a470c670163c342253594f1b12b2ba27590
Manal <andnothingevrything@gmail.com>
2023-06-01 21:50:17 +0200

    modification de l'emplacement des fonctions dans le fichier

--- forces.c
+++ forces.c
@@ -14,68 +14,7 @@ typedef struct {
 k : la constante de rappel en Newton par mètre (N/m),
 x : le d ́eformation en mètre (m).
 */
-Vector3 f_rappel(float k, Vector3 deformation) {
-    Vector3 force_rappel;
-    force_rappel.x = -k * deformation.x;
-    force_rappel.y = -k * deformation.y;
-    force_rappel.z = -k * deformation.z;
-    return force_rappel;
-}
 
-/*       Forces extérieures      */
-/*Calcul de la force de frottement du fluide en Newton.
-viscosite : la viscosité du fluide en Pascal seconde (P a · s),
-r : rayon de la boule en m`etre (m),
-v : vitesse de la boule en m`etre par seconde (m.s−1)
-*/
-Vector3 f_resist(float viscosite, float rayon, Vector3 vitesse) {
-    Vector3 force_resist;
-    float constante = -6 * M_PI * viscosite * rayon;
-    force_resist.x = constante * vitesse.x;
-    force_resist.y = constante * vitesse.y;
-    force_resist.z = constante * vitesse.z;
-    return force_resist;
-}
-
-
-/* Calcul de la force de pesanteur 
-m : masse de l'objet
-*/
-Vector3 f_pesanteur(float masse) {
-    Vector3 force_pesanteur;
-    force_pesanteur.x = 0.0;
-    force_pesanteur.y = 0.0;
-    force_pesanteur.z = -masse * g;
-    return force_pesanteur;
-}
-
-/* Calcul de la force totale */
-//Vector3 * calculer_
-/*Autres forces = autres forces qui s'appliquent sur le tissus (vent)*/
-/*Vector3 calculer_force_totale(Vector3 * tableau_f_rappel, float viscosite, float rayon, Vector3 vitesse, float masse, Vector3 autres_forces) {
-    Vector3 force_totale;
-
-   // Vector3 force_rappel = f_rappel(k, deformation);
-    Vector3 force_pesanteur = f_pesanteur(masse);
-    
-    //Calcul de la force + application résistance
-    Vector3 force_resist = f_resist(viscosite, rayon, vitesse);// vitesse précédente
-
-    force_totale.x = force_rappel.x + force_resist.x + force_pesanteur.x + autres_forces.x;
-    force_totale.y = force_rappel.y + force_resist.y + force_pesanteur.y + autres_forces.y;
-    force_totale.z = force_rappel.z + force_resist.z + force_pesanteur.z + autres_forces.z ;
-
-    return force_totale;
-}
-
-*/
-
-//calculs ressort 
-/* tableau_poids // tableau ressort (avec l'indice des deux relier)// 
-
-Longueure ressort au repos*/
-
-//Vector3 * calculs_ressorts
 Vector3 calculs_ressort(Vector3 Pa, Vector3 Pb, float k, float longueur_ressort_repos) {
     Vector3 direction;
     Vector3 direction_normalisee;
@@ -129,9 +68,63 @@ void tables_forces_ressorts(Vector3* tableau_poids, int** tableau_ressorts, int 
         tableau_forces_total_appliquer_sur_les_points_R[ressort[1]] = nouvelle_force_P2;
     }
 }
+/*
+Vector3 f_rappel(float k, Vector3 deformation) {
+    Vector3 force_rappel;
+    force_rappel.x = -k * deformation.x;
+    force_rappel.y = -k * deformation.y;
+    force_rappel.z = -k * deformation.z;
+    return force_rappel;
+}*/"
+"
+
+/*       Forces extérieures      */
+/*Calcul de la force de frottement du fluide en Newton.
+viscosite : la viscosité du fluide en Pascal seconde (P a · s),
+r : rayon de la boule en m`etre (m),
+v : vitesse de la boule en m`etre par seconde (m.s−1)
+*/
+Vector3 f_resist(float viscosite, float rayon, Vector3 vitesse) {
+    Vector3 force_resist;
+    float constante = -6 * M_PI * viscosite * rayon;
+    force_resist.x = constante * vitesse.x;
+    force_resist.y = constante * vitesse.y;
+    force_resist.z = constante * vitesse.z;
+    return force_resist;
+}
 
 
+/* Calcul de la force de pesanteur 
+m : masse de l'objet
+*/
+Vector3 f_pesanteur(float masse) {
+    Vector3 force_pesanteur;
+    force_pesanteur.x = 0.0;
+    force_pesanteur.y = 0.0;
+    force_pesanteur.z = -masse * g;
+    return force_pesanteur;
+}
 
+/* Calcul de la force totale */
+//Vector3 * calculer_
+/*Autres forces = autres forces qui s'appliquent sur le tissus (vent)*/
+/*Vector3 calculer_force_totale(Vector3* tableau_poids, int taille_tableau_poids, int** tableau_ressorts, int taille_tableau_ressorts, float viscosite, float rayon, Vector3 autres_forces) {
+    Vector3* force_totale;
+    Vector3 force_pesanteur = f_pesanteur(masse);// La masse est constante donc je ne le fait qu'une fois(en cas de masses différentes, le faire dans la boucle)
+    
+    Vector3* tableau_poids, int** tableau_ressorts, int taille_tableau_ressorts
+
+    //Calcul de la force + application résistance
+    Vector3 force_resist = f_resist(viscosite, rayon, vitesse);// vitesse précédente
+
+    force_totale.x = force_rappel.x + force_resist.x + force_pesanteur.x + autres_forces.x;
+    force_totale.y = force_rappel.y + force_resist.y + force_pesanteur.y + autres_forces.y;
+    force_totale.z = force_rappel.z + force_resist.z + force_pesanteur.z + autres_forces.z ;
+
+    return force_totale;
+}
+*/
+/** ne pas oublier de tester si les ressorts relie des poids existant au chargement du  model (voir si chaque points sont < à taille tableau poids)*/
 
 int main(void) {
     // Test
